3123aea5671b64043c870aaaabda30ea0142ff3f
FhikarZul <Fhikarzul18@gmail.com>
2022-02-15 22:38:19 +0800

    Add files via upload

---
+++ assets/js/translator.js
@@ -0,0 +1,20 @@
+function googleTranslateElementInit() {
+		new google.translate.TranslateElement({
+			pageLanguage: 'id',
+			autoDisplay: false,
+			layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+		}, 'google_translate_element');
+		function changeGoogleStyles() {
+			if($('.goog-te-menu-frame').contents().find('.goog-te-menu2').length) {
+					$('.goog-te-menu-frame').contents().find('.goog-te-menu2').css({
+					'max-width':'100%',
+					'overflow-x':'auto',
+					'box-sizing':'border-box',
+					'height':'auto'
+				});
+			} else {
+				setTimeout(changeGoogleStyles, 50);
+				}
+			}
+			changeGoogleStyles();
+		}
---
+++ index.php
@@ -0,0 +1,390 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Fhikar - Portofolio</title>
+
+	<!-- Meta Data -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="format-detection" content="telephone=no"/>
+    <meta name="format-detection" content="address=no"/>
+    <meta name="author" content="Fhikar" />
+    <meta name="description" content="Fhikar " />
+
+    <!-- Twitter data -->
+    <!-- <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@ArtTemplates">
+    <meta name="twitter:title" content="vCard">
+    <meta name="twitter:description" content="vCard">
+    <meta name="twitter:image" content="assets/images/social.html"> -->
+
+    <!-- Open Graph data -->
+    <meta property="og:title" content="Fhikar - Portfolio" />
+    <meta property="og:type" content="Portfolio" />
+    <meta property="og:url" content="your url website" />
+    <meta property="og:image" content="assets/images/social.html" />
+    <meta property="og:description" content="Portofolio" />
+    <meta property="og:site_name" content="Fhikar - Portofolio" />
+
+	<!-- Favicons -->
+	<link rel="apple-touch-icon" sizes="144x144" href="assets/images/favicons/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="assets/images/favicons/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="assets/images/favicons/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="57x57" href="assets/images/favicons/apple-touch-icon-57x57.png">
+	<link rel="shortcut icon" href="assets/images/favicons/favicon.png" type="image/png">
+
+    <!-- Styles -->
+	<link rel="stylesheet" type="text/css" href="assets/styles/style.css"/>
+	<link rel="stylesheet" type="text/css" href="assets/styles/style-dark.css"/>
+    <link rel="stylesheet" type="text/css" href="assets/demo/style-demo.css"/>
+    <link rel="stylesheet" type="text/css" href="assets/styles/translator.css"/>
+	
+</head>
+<body>
+    <!-- Preloader -->
+    <div class="preloader">
+	    <div class="preloader__wrap">
+		    <div class="circle-pulse">
+                <div class="circle-pulse__1"></div>
+                <div class="circle-pulse__2"></div>
+            </div>
+		    <div class="preloader__progress"><span></span></div>
+		</div>
+	</div>
+
+    <main class="main">
+				
+	    <!-- Header Image -->
+		<div class="header-image">
+		    <div class="js-parallax" style="background-image: url(assets/img/bg.jpeg);"></div>
+		</div>
+
+		
+	    <div class="container gutter-top">
+		    <!-- Header -->
+		    <header class="header box">
+			    <div class="header__left">
+				    <div class="header__photo">
+					    <img class="header__photo-img" src="assets/img/avatar.png">
+					</div>
+					<div class="header__base-info">
+					    <h4 class="title titl--h4">Sulfikar</h4>
+						<div class="status">Mobile Developer</div>
+						<ul class="header__social">
+						    <li><a href="https://www.facebook.com/"><i class="font-icon icon-facebook1"></i></a></li>
+						    <li><a href="https://www.twitter.com/"><i class="font-icon icon-twitter1"></i></a></li>
+							<li><a href="https://www.instagram.com/"><i class="font-icon icon-instagram1"></i></a></li>
+						</ul>
+						<!-- translator -->
+						<div id="google_translate_element"></div>
+					</div>
+				</div>
+				<div class="header__right">
+				    <ul class="header__contact">
+					    <li><span class="overhead">Email</span>fhikarzul18@gmail.com</li>
+						<li><span class="overhead">Phone</span>08875141408</li>
+					</ul>
+					<ul class="header__contact">
+					    <li><span class="overhead">Birthday</span>18 Oktober, 1997</li>
+						<li><span class="overhead">Location</span>Makassar, Sulawesi Selatan, Indonesia</li>
+					</ul>
+				</div>
+			</header>
+			
+		    <div class="row sticky-parent">
+			    <!-- Sidebar nav -->
+                <aside class="col-12 col-md-12 col-lg-2">
+				    <div class="sidebar box sticky-column">
+	                    <ul class="nav js-tabs">
+                            <li class="nav__item"><a class="active" href="#about-tab"><i class="icon-user"></i>About</a></li>
+							<li class="nav__item"><a href="#resume-tab"><i class="icon-file-text"></i>Resume</a></li>
+                            <li class="nav__item"><a href="#works-tab"><i class="icon-codesandbox"></i>Portofolio</a></li>
+                        </ul>
+					</div>
+		        </aside>
+		        
+				<!-- Content -->
+		        <div class="col-12 col-md-12 col-lg-10">
+				    <div class="box box-content" id="content">
+                          
+			            <div class="content">
+						    <!-- ABOUT -->
+						    <div id="about-tab" class="tabcontent active">
+                                <div class="pb-0 pb-sm-2">
+		                            <h1 class="title title--h1 first-title title__separate">Tentang Saya</h1>
+						            <p>Sebagai mobile developer dari tahun 2019 yang memiliki keterampilan baik dalam melakukan analisis, mengimplementasi sistem perangkat lunak untuk menyelesaikan suatu permasalahan. Dapat bekerjasama dengan tim, memiliki keterampilan komunikasi yang baik serta teliti dan bertanggung jawab terhadap pekerjaan.</p>
+					            </div>
+						
+								<!-- What -->
+								<div class="mt-1">
+									<h2 class="title title--h3">What I'm Doing</h2>
+									<div class="row">
+
+										<!-- Case Item -->
+										<div class="col-12 col-lg-6">
+											<div class="case-item">
+												<img class="case-item__icon" src="assets/img/icon-app.svg" alt="" />
+												<div>
+													<h3 class="title title--h4">Mobile Apps</h3>
+													<p class="case-item__caption">Pengembangan profesional aplikasi untuk Android.</p>
+												</div>
+										   </div>
+										</div>
+										
+										<!-- Case Item -->
+										<div class="col-12 col-lg-6">
+											<div class="case-item">
+												<img class="case-item__icon" src="assets/img/icon-dev.svg" alt="" />
+												<div>
+													<h3 class="title title--h4">Web Development</h3>
+													<p class="case-item__caption">Pengembangan situs web berkualitas tinggi di tingkat profesional.</p>
+												</div>
+											</div>
+										</div>
+										
+										
+										
+										
+									</div>	
+								</div>
+						
+						    </div>
+							
+							<!-- RESUME -->
+							<div id="resume-tab" class="tabcontent">
+                                <div class="pb-2">
+		                            <h1 class="title title--h1 first-title title__separate">Resume</h1>
+					            </div>
+						
+								<!-- Experience -->
+								<div class="row">
+									<div class="col-12">
+										<h2 class="title title--h3"><img class="title-icon" src="assets/img/icon-education.svg" alt="" /> Education</h2>
+										<div class="timeline">
+											<!-- Item -->
+											<article class="timeline__item">
+												<h5 class="title title--h5 timeline__title">Universitas Muslim Indonesia</h5>
+												<span class="timeline__period">2016 — 2020</span>
+												<p class="timeline__description">Fakultas Ilmu Komputer, Jurusan Teknik Informatika.</p>
+											</article>
+												
+											<!-- Item -->
+											<article class="timeline__item">
+												<h5 class="title title--h5 timeline__title">SMAN 1 Liliriaja</h5>
+												<span class="timeline__period">2013 — 2016</span>
+												<p class="timeline__description">Jurusan Ilmu Pengetahuan Alam.</p>
+											</article>
+									
+										</div>	
+									</div>
+										
+									<div class="col-12">
+										<h2 class="title title--h3"><img class="title-icon" src="assets/img/icon-experience.svg" alt="" /> Employment History</h2>
+										<div class="timeline">
+											<!-- Item -->
+											<article class="timeline__item">
+												<h5 class="title title--h5 timeline__title">Freelancer - Aplikasi Android dan Website</h5>
+												<span class="timeline__period">2019 — Sekarang</span>
+												<p class="timeline__description">Menerima jasa pembuatan aplikasi Android dan Website baik skala swasta maupun pemerintahan.</p>
+											</article>
+												
+											<!-- Item -->
+											<article class="timeline__item">
+												<h5 class="title title--h5 timeline__title">PT. Liny Jaya Informatika</h5>
+												<span class="timeline__period">2021 — 2022</span>
+												<p class="timeline__description">Sebagai Mobile Developer.</p>
+											</article>
+											
+										</div>
+									</div>
+								</div>
+								
+								<!-- Skills -->
+								<div class="row">
+									<div class="col-12">
+										<h2 class="title title--h3">My Skills</h2>
+										<div class="box box__border">
+											<!-- Progress -->
+											<div class="progress">
+												<div class="progress-bar" role="progressbar" aria-valuenow="99" aria-valuemin="0" aria-valuemax="100">
+													<div class="progress-text"><span>Dart</span></div>
+												</div>
+											</div>
+												
+											<!-- Progress -->
+											<div class="progress">
+												<div class="progress-bar" role="progressbar" aria-valuenow="99" aria-valuemin="0" aria-valuemax="100">
+													<div class="progress-text"><span>Java</span></div>
+												</div>
+											</div>
+
+											<!-- Progress -->
+											<div class="progress">
+												<div class="progress-bar" role="progressbar" aria-valuenow="99" aria-valuemin="0" aria-valuemax="100">
+													<div class="progress-text"><span>PHP</span></div>
+												</div>
+											</div>
+
+											<!-- Progress -->
+											<div class="progress">
+												<div class="progress-bar" role="progressbar" aria-valuenow="99" aria-valuemin="0" aria-valuemax="100">
+													<div class="progress-text"><span>C++</span></div>
+												</div>
+											</div>
+
+											<!-- Progress -->
+											<div class="progress">
+												<div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+													<div class="progress-text"><span>C</span></div>
+												</div>
+											</div>
+
+											<!-- Progress -->
+											<div class="progress">
+												<div class="progress-bar" role="progressbar" aria-valuenow="99" aria-valuemin="0" aria-valuemax="100">
+													<div class="progress-text"><span>MySql</span></div>
+												</div>
+											</div>
+
+											<!-- Progress -->
+											<div class="progress">
+												<div class="progress-bar" role="progressbar" aria-valuenow="99" aria-valuemin="0" aria-valuemax="100">
+													<div class="progress-text"><span>Flutter</span></div>
+												</div>
+											</div>
+
+											<!-- Progress -->
+											<div class="progress">
+												<div class="progress-bar" role="progressbar" aria-valuenow="99" aria-valuemin="0" aria-valuemax="100">
+													<div class="progress-text"><span>Android Studio</span></div>
+												</div>
+											</div>
+
+											<!-- Progress -->
+											<div class="progress">
+												<div class="progress-bar" role="progressbar" aria-valuenow="99" aria-valuemin="0" aria-valuemax="100">
+													<div class="progress-text"><span>Laravel</span></div>
+												</div>
+											</div>
+
+											<!-- Progress -->
+											<div class="progress">
+												<div class="progress-bar" role="progressbar" aria-valuenow="99" aria-valuemin="0" aria-valuemax="100">
+													<div class="progress-text"><span>Firebase</span></div>
+												</div>
+											</div>
+
+										</div>
+									</div>
+								</div>
+							</div>
+							
+							<!-- WORKS -->
+							<div id="works-tab" class="tabcontent">
+                                <div class="pb-2">
+		                            <h1 class="title title--h1 first-title title__separate">Portofolio</h1>
+					            </div>
+						
+								<!-- Filter -->
+								<div class="select">
+									<span class="placeholder">Select category</span>
+									<ul class="filter">
+										<li class="filter__item">Category</li>
+										<li class="filter__item active" data-filter="*"><a class="filter__link active" href="#filter">All</a></li>
+										<li class="filter__item" data-filter=".category-android"><a class="filter__link" href="#filter">Android</a></li>
+										<li class="filter__item" data-filter=".category-web"><a class="filter__link" href="#filter">Web</a></li>
+									</ul>
+									<input type="hidden" name="changemetoo"/>
+								</div>
+
+								<!-- Gallery -->
+								<div class="gallery-grid js-grid-row js-filter-container">
+									<div class="gutter-sizer"></div>
+									<!-- Item 1 -->
+									<figure class="gallery-grid__item category-android">
+										<div class="gallery-grid__image-wrap">
+											<img class="gallery-grid__image cover lazyload" src="assets/img/apps/image_01.png" data-zoom alt="" />
+										</div>
+										<figcaption class="gallery-grid__caption">
+											<h4 class="title title--h6 gallery-grid__title">RAISA</h4>
+											<h4 class="title title--h6 gallery-grid__title">(Dinas Peternakan dan Kesehatan Hewan SulSel)</h4>
+											<span class="gallery-grid__category">Aplikasi berbasis android yang berfungsi untuk mempertemukan peternak dengan inseminator.</span>
+										</figcaption>
+									</figure>
+
+									<!-- Item 2 -->
+									<figure class="gallery-grid__item category-android">
+										<div class="gallery-grid__image-wrap">
+											<img class="gallery-grid__image cover lazyload" src="assets/img/apps/image_02.png" data-zoom alt="" />
+										</div>
+										<figcaption class="gallery-grid__caption">
+											<h4 class="title title--h6 gallery-grid__title">Mobile Absensi Kehadiran</h4>
+											<h4 class="title title--h6 gallery-grid__title">(DPRD Sulawesi Selatan)</h4>
+											<span class="gallery-grid__category">Aplikasi berbasis android yang berfungsi untuk sistem absensi kehadiran non ASN di DPRD Sulawesi Selatan.</span>
+										</figcaption>
+									</figure>
+
+									<!-- Item 3 -->
+									<figure class="gallery-grid__item category-android">
+										<div class="gallery-grid__image-wrap">
+											<img class="gallery-grid__image cover lazyload" src="assets/img/apps/image_03.png" data-zoom alt="" />
+										</div>
+										<figcaption class="gallery-grid__caption">
+											<h4 class="title title--h6 gallery-grid__title">Mobile Absensi Rapat</h4>
+											<h4 class="title title--h6 gallery-grid__title">(DPRD Sulawesi Selatan)</h4>
+											<span class="gallery-grid__category">Aplikasi berbasis android yang berfungsi untuk sistem absensi rapat dengan cara memindai QR Code.</span>
+										</figcaption>
+									</figure>
+
+									<!-- Item 4 -->
+									<figure class="gallery-grid__item category-web">
+										<div class="gallery-grid__image-wrap">
+											<img class="gallery-grid__image cover lazyload" src="assets/img/apps/image_04.png" data-zoom alt="" />
+										</div>
+										<figcaption class="gallery-grid__caption">
+											<h4 class="title title--h6 gallery-grid__title">Web Produk UMKM</h4>
+											<h4 class="title title--h6 gallery-grid__title">(Kecamatan Parangloe, Kabupaten Gowa)</h4>
+											<span class="gallery-grid__category">Website yang mempromosikan produk-produk UMKM yang ada di Kecamatan Parangloe Kabupaten Gowa.</span>
+										</figcaption>
+									</figure>
+
+							
+								</div><!-- Gallery End -->
+							</div>
+							
+						
+							
+						</div>
+					</div>
+					<!-- Footer -->
+					<footer class="footer">© <?php echo date("Y");?> Fhikar - Portofolio</footer>
+		        </div>
+			</div>
+		</div>	
+    </main>
+
+    <div class="back-to-top"></div>
+
+    <!-- SVG masks -->
+    <svg class="svg-defs">
+        <clipPath id="avatar-box">
+            <path d="M1.85379 38.4859C2.9221 18.6653 18.6653 2.92275 38.4858 1.85453 56.0986.905299 77.2792 0 94 0c16.721 0 37.901.905299 55.514 1.85453 19.821 1.06822 35.564 16.81077 36.632 36.63137C187.095 56.0922 188 77.267 188 94c0 16.733-.905 37.908-1.854 55.514-1.068 19.821-16.811 35.563-36.632 36.631C131.901 187.095 110.721 188 94 188c-16.7208 0-37.9014-.905-55.5142-1.855-19.8205-1.068-35.5637-16.81-36.63201-36.631C.904831 131.908 0 110.733 0 94c0-16.733.904831-37.9078 1.85379-55.5141z"/>
+        </clipPath>
+        <clipPath id="avatar-hexagon">
+             <path d="M0 27.2891c0-4.6662 2.4889-8.976 6.52491-11.2986L31.308 1.72845c3.98-2.290382 8.8697-2.305446 12.8637-.03963l25.234 14.31558C73.4807 18.3162 76 22.6478 76 27.3426V56.684c0 4.6805-2.5041 9.0013-6.5597 11.3186L44.4317 82.2915c-3.9869 2.278-8.8765 2.278-12.8634 0L6.55974 68.0026C2.50414 65.6853 0 61.3645 0 56.684V27.2891z"/>
+        </clipPath>		
+    </svg>
+
+
+	<!-- JavaScripts -->
+	<script src="assets/js/jquery-3.4.1.min.js"></script>
+	<script src="assets/js/plugins.min.js"></script>
+    <script src="assets/js/common.js"></script>
+	<script src="assets/js/translator.js"></script>
+	<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+    
+</body>
+
+</html>
